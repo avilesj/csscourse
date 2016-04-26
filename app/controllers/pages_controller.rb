8fc9cd4e6f5bc7e94e987c71bea37636a28d8e5b
Jose Aviles <jlapmail@gmail.com>
2016-04-26 01:15:51 -0400

    Added map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
     def index
+      @latitude = 18.5015488
+      @longitude = -69.9420843
+      @zoom = 17
+      @title = "Centro de Atención Integral Familiar"
+      @width = 36
+      @height = 36
+
+      @hash = [:lat => @latitude, :lng => @longitude, :picture => {
+                      :url    => "#{view_context.asset_path('caifsmall.png')}",
+                       :width  => @width,
+                       :height => @height,
+                       }, :infowindow => @title]
     end
-    
-    def aboutme
-    end
-    
-    def aboutus
-    end
-    
-    def contact
-    end
+
 end
